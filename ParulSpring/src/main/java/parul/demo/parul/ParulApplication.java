50f68ce5cb72928ba040298d557ff78c5d6a445a
parulchawla894 <parulchawla893@gmail.com>
2019-10-14 16:02:40 +0530

    first commit

---
+++ ParulSpring/src/main/java/parul/demo/parul/ParulApplication.java
@@ -0,0 +1,13 @@
+package parul.demo.parul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParulApplication.class, args);
+    }
+
+}
---
+++ ParulSpring/src/test/java/parul/demo/parul/ParulApplicationTests.java
@@ -0,0 +1,16 @@
+package parul.demo.parul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
